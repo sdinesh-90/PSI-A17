ab8e0a3927fc41799af18f20d88b3a140ed5c7d6
Dakshita <dakshitaminocha@metamation.com>
2024-01-25 14:14:05 +0530

    Changing evaluator code to recognise REAL values

--- Parser/Evaluate.cs
+++ Parser/Evaluate.cs
@@ -2,24 +2,24 @@
 using static Token.E;
 
 // An expression evaluator, implementing using the visitor pattern
-class ExprEvaluator : Visitor<int> {
-   public ExprEvaluator (Dictionary<string, int> dict) => mDict = dict;
-   Dictionary<string, int> mDict;
+class ExprEvaluator : Visitor<double> {
+   public ExprEvaluator (Dictionary<string, double> dict) => mDict = dict;
+   Dictionary<string, double> mDict;
 
-   public override int Visit (NLiteral literal)
-      => int.Parse (literal.Value.Text);
+   public override double Visit (NLiteral literal)
+      => double.Parse (literal.Value.Text);
 
-   public override int Visit (NIdentifier identifier)
+   public override double Visit (NIdentifier identifier)
       => mDict[identifier.Name.Text];
 
-   public override int Visit (NUnary unary) {
-      int d = unary.Expr.Accept (this);
+   public override double Visit (NUnary unary) {
+      double d = unary.Expr.Accept (this);
       if (unary.Op.Kind == SUB) d = -d;
       return d;
    }
 
-   public override int Visit (NBinary binary) {
-      int a = binary.Left.Accept (this), b = binary.Right.Accept (this);
+   public override double Visit (NBinary binary) {
+      double a = binary.Left.Accept (this), b = binary.Right.Accept (this);
       return binary.Op.Kind switch {
          ADD => a + b, SUB => a - b, MUL => a * b, DIV => a / b,
          _ => throw new NotImplementedException ()
--- Parser/Start.cs
+++ Parser/Start.cs
@@ -5,8 +5,8 @@ static class Start {
       var parser = new Parser (new Tokenizer (Expr0));
       var node = parser.Parse ();
 
-      var dict = new Dictionary<string, int> () { ["five"] = 5, ["two"] = 2 };
-      int value = node.Accept (new ExprEvaluator (dict));
+      var dict = new Dictionary<string, double> () { ["five"] = 5, ["two"] = 2 };
+      double value = node.Accept (new ExprEvaluator (dict));
       Console.WriteLine ($"Value = {value}");
 
       var sb = node.Accept (new ExprILGen ());
@@ -22,5 +22,5 @@ static class Start {
    }
 
    static string Expr0
-      = "(3 + 2) * 4 - 17 * -five * (two + 1 + 4 + 5)";
+      = "(3 + 2.5) * 4 - 17 * -five * (two + 1 + 4 + 5)";
 }
