e60a5759a130f08cc88c9cb6ae162badfa3f5daf
Dakshita <dakshitaminocha@metamation.com>
2024-01-31 12:21:19 +0530

    using constraints in ExprTyper as in A18

--- Parser/ExprTyper.cs
+++ Parser/ExprTyper.cs
@@ -11,16 +11,16 @@ class ExprTyper : Visitor<NType> {
    public override NType Visit (NUnary unary)
       => unary.Type = unary.Expr.Accept (this);
 
-   public override NType Visit (NBinary binary)
-      => binary.Type = (binary.Left.Accept (this), binary.Op.Kind, binary.Right.Accept (this)) switch {
-         (NType.Int, ADD, NType.Int) => NType.Int,
-         (NType.Int, SUB, NType.Int) => NType.Int,
-         (NType.Int, MUL, NType.Int) => NType.Int,
-         (NType.Int, DIV, NType.Int) => NType.Real,
-         (NType.String, ADD, NType.String) => NType.String,
+   public override NType Visit (NBinary binary) {
+      NType a = binary.Left.Accept (this), b = binary.Right.Accept (this);
+      return binary.Type = (a, binary.Op.Kind, b) switch {
+         (NType.Real or NType.Int, ADD or SUB or MUL or DIV, NType.Real or NType.Int) when a == b => a,
          (NType.Real or NType.Int, ADD or SUB or MUL or DIV, NType.Real or NType.Int) => NType.Real,
+         (NType.String, ADD, _) => NType.String,
+         (_, ADD, NType.String) => NType.String,
          (NType.Real or NType.Int, LT or GEQ or GT or LEQ or EQ or NEQ, NType.Real or NType.Int) => NType.Bool,
          (NType.String, LT or GT or EQ or NEQ, NType.String) => NType.Bool,
          _ => NType.Error
       };
+   }
 }
