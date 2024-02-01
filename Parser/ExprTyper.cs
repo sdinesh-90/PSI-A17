74c58065404db436587112ab45a9210345e9f99a
Dakshita <dakshitaminocha@metamation.com>
2024-02-01 09:48:35 +0530

    Changes in Expr Typer as per PR#1

--- Parser/ExprTyper.cs
+++ Parser/ExprTyper.cs
@@ -2,8 +2,10 @@
 namespace PSI;
 class ExprTyper : Visitor<NType> {
    public override NType Visit (NLiteral literal)
-      => literal.Type = literal.Value.Kind is STRING ? NType.String :
-                        literal.Value.Kind is INTEGER ? NType.Int : NType.Real;
+      => literal.Type = literal.Value.Kind switch {
+         STRING => NType.String, INTEGER => NType.Int,
+         REAL => NType.Real, _ => NType.Error
+      };
 
    public override NType Visit (NIdentifier identifier)
       => identifier.Type = NType.Int;
@@ -14,10 +16,8 @@ class ExprTyper : Visitor<NType> {
    public override NType Visit (NBinary binary) {
       NType a = binary.Left.Accept (this), b = binary.Right.Accept (this);
       return binary.Type = (a, binary.Op.Kind, b) switch {
-         (NType.Real or NType.Int, ADD or SUB or MUL or DIV, NType.Real or NType.Int) when a == b => a,
-         (NType.Real or NType.Int, ADD or SUB or MUL or DIV, NType.Real or NType.Int) => NType.Real,
-         (NType.String, ADD, _) => NType.String,
-         (_, ADD, NType.String) => NType.String,
+         (NType.Real or NType.Int, ADD or SUB or MUL or DIV, NType.Real or NType.Int) => a == b ? a : NType.Real, 
+         (NType.String or _, ADD, _ or NType.String) => NType.String,
          (NType.Real or NType.Int, LT or GEQ or GT or LEQ or EQ or NEQ, NType.Real or NType.Int) => NType.Bool,
          (NType.String, LT or GT or EQ or NEQ, NType.String) => NType.Bool,
          _ => NType.Error
